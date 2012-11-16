12717bd07b9684991d59b3286b1dc571bbb62cbb
Craig Pentrack <cpentra1@cs.swarthmore.edu>
2012-11-15 20:14:19 -0500

    added client_handshake to bt_connect

--- bt_connect.c
+++ bt_connect.c
@@ -76,5 +76,28 @@ void extract_attributes(be_node *node, bt_info_t *info){
 
 }
 
+  //function for building message
+int client_handshake(int sockfd, char* fname, struct sockaddr_in* sockaddr) {
+  unsigned char nineteen = "19";
+  unsigned char name[19] = "BitTorrent Protocol";
+  
+  // setting extensions to 0
+  unsigned char ext[8];
+  memset(ext, 0, 8);
+
+  //SHA1 hash of filename for info-hash
+  unsigned char info_hash[20];
+  SHA1((unsigned char *) fname, strlen(fname), info_hash);
+
+  //setting your peer id
+  char *ip; 
+  unsigned short port; 
+  char id[20];
+
+  calc_id(ip, port, id);
+
+  return; //will be 1 or 0
+}
+
 
 
